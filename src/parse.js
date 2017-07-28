1911b3f5ccf916149b8583b02534aed687173807
ChalkPE <chalk@chalk.pe>
2017-07-28 17:42:47 +0900

    Add homepage and cBooking info

--- src/parse.js
+++ src/parse.js
@@ -37,7 +37,13 @@ function parse () {
   const addressNodes = infoArea.querySelectorAll('.list_item_address span.addr')
   const addresses = Array.prototype.map.call(addressNodes, node => node.textContent)
 
-  return { name, category, nBooking, tel, addresses }
+  const homepageAnchors = infoArea.querySelectorAll('.list_item_homepage a')
+  const homepages = Array.prototype.map.call(homepageAnchors, node => node.getAttribute('href'))
+
+  const convenienceNode = infoArea.querySelector('.list_item_convenience .convenience')
+  const cBooking = convenienceNode && convenienceNode.textContent.indexOf('예약') >= 0
+
+  return { name, category, nBooking, cBooking, tel, addresses, homepages }
 }
 
 main()
